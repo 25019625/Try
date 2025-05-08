b0298c02fc1093d9d57e6770152c29b5737dc85a
unknown <25019625@mvula.univen.ac.za>
2025-05-08 22:38:02 +0200

    first commit

---
+++ pkgtry/Try.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package pkgtry;
+
+/**
+ *
+ * @author admin
+ */
+public class Try {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+int user = 17;
+if (user < 19){
+System.out.println("The user is less than 19");
+}
+ if (user == 19 || user == 17){
+System.out.println("The user is either 19 or 17");
+}
+}
+}
+
+
